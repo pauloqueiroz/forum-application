3db0c58a191d2408c1def1f8c1582e179eb27422
Paulo Sérgio <pauloqueiroz16@gmail.com>
2020-11-23 13:27:32 -0300

    Configurando um banco a parte vazio para ser utilizado nos testes unitários

--- src/test/java/br/com/alura/forum/repository/CursoRepositoryTest.java
+++ src/test/java/br/com/alura/forum/repository/CursoRepositoryTest.java
@@ -5,21 +5,31 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import br.com.alura.forum.modelo.Curso;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
+@ActiveProfiles("test")
 public class CursoRepositoryTest {
 
 	@Autowired
 	private CursoRepository repository;
 	
+	@Autowired
+	private TestEntityManager em;
+	
 	@Test
 	public void deveriaCarregarCursoCadastradoPeloNome() {
 		String nomeCurso = "Spring Boot";
-		// Os cursos cadastrados estao no arquivo data.sql
+		Curso novoCurso = new Curso();
+		novoCurso.setNome(nomeCurso);
+		novoCurso.setCategoria("Programação");
+		em.persist(novoCurso);
+		
 		Curso curso = repository.findByNome(nomeCurso);
 		Assert.assertNotNull(curso);
 		Assert.assertEquals(curso.getNome(), nomeCurso);
