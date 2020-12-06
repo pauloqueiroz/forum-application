42bc4b4a9e45c6eb44a6b5eea0635a2ee50e683c
Paulo Sérgio <pauloqueiroz16@gmail.com>
2020-12-06 01:39:15 -0300

    adicionando parâmetro Authorization à documentação

--- src/main/java/br/com/alura/forum/config/swagger/SwaggerConfigurations.java
+++ src/main/java/br/com/alura/forum/config/swagger/SwaggerConfigurations.java
@@ -1,12 +1,16 @@
 package br.com.alura.forum.config.swagger;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import br.com.alura.forum.modelo.Usuario;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -21,6 +25,14 @@ public class SwaggerConfigurations {
 					.apis(RequestHandlerSelectors.basePackage("br.com.alura.forum"))
 					.paths(PathSelectors.ant("/**"))
 					.build()
-					.ignoredParameterTypes(Usuario.class);
+					.ignoredParameterTypes(Usuario.class)
+					.globalOperationParameters(Arrays.asList(
+							new ParameterBuilder()
+							.name("Authorization")
+							.description("Token de acesso")
+							.modelRef(new ModelRef("string"))
+							.parameterType("header")
+							.required(false)
+							.build()));
 	}
 }
